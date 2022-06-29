2f6baa44199d8ac6f83088d597a3cffe239fd144
Caleb Rider <donutclubnft@gmail.com>
2022-06-29 14:36:59 -0400

    added wallet connection and address display

--- front-end/components/Navbar.js
+++ front-end/components/Navbar.js
@@ -1,11 +1,11 @@
 import styles from '../styles/Home.module.css'
 
-export default function Navbar() {
+export default function Navbar({connectWallet}) {
 
     return(
         <div className={styles.nav}>
             <h1>NFT Minting Bot</h1>
-            <button className={styles.btn}>Connect Wallet</button>
+            <button id='walletButton' onClick={connectWallet} className={styles.btn}>Connect Wallet</button>
         </div>
     );
 
